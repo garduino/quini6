fd9c9f1c9c064b91251fb0272a3f705b00d5f14a
German Arduino <garduino@gmail.com>
2015-09-12 15:00:21 -0300

    Intermediate Saving.

--- quini6.js
+++ quini6.js
@@ -2,28 +2,37 @@ if (Meteor.isClient) {
   // counter starts at 0
   //Session.setDefault('winners', 0);
 
-  Template.try.helpers({
+
+  Template.try.created = function () {
+    // here `this` refers to template instance
+
+    var winners = [0, 0, 0, 0, 0, 0];
+
+    for (step = 0; step < 6; step++) {
+      // Calculo random según http://www.w3schools.com/jsref/jsref_random.asp
+      var number = Math.floor((Math.random() * 45) + 1);
+      winners[step] = number;
+    }
+
+    // var sortedWinners = winners.sort(ascending);
+    //console.log(sortedWinners);
+    // return sortedWinners;
+    this.sortedWinners = winners.sort(ascending);
+
+    // Functions
+
+    // Order a numeric array, taken from http://www.aprenderaprogramar.com/index.php?option=com_content&view=article&id=834:funciones-arrays-javascript-push-sort-ordenar-numeros-concat-join-pop-shift-slice-splice-etc-cu01153e&catid=78:tutorial-basico-programador-web-javascript-desde-&Itemid=206
+
+    function ascending (elem1, elem2) { return elem1 - elem2; }
+
+    // return Session.get('sortedWinners');
+  };
+
+
+Template.try.helpers({
     sortedWinners: function () {
+      return Template.instance().sortedWinners;
 
-      var winners = [0, 0, 0, 0, 0, 0];
-
-      for (step = 0; step < 6; step++) {
-        // Calculo random según http://www.w3schools.com/jsref/jsref_random.asp
-        var number = Math.floor((Math.random() * 45) + 1);
-        winners[step] = number;
-      }
-
-      var sortedWinners = winners.sort(ascending);
-      //console.log(sortedWinners);
-      return sortedWinners;
-
-      // Functions
-
-      // Order a numeric array, taken from http://www.aprenderaprogramar.com/index.php?option=com_content&view=article&id=834:funciones-arrays-javascript-push-sort-ordenar-numeros-concat-join-pop-shift-slice-splice-etc-cu01153e&catid=78:tutorial-basico-programador-web-javascript-desde-&Itemid=206
-
-      function ascending (elem1, elem2) { return elem1 - elem2; }
-
-      // return Session.get('sortedWinners');
     }
   });
 
@@ -31,8 +40,8 @@ if (Meteor.isClient) {
     'click button': function () {
       // increment the counter when button is clicked
       //Session.set('sortedWinners', Session.get('sortedWinners'));
-      Session.set('sortedWinners');
-
+      // alert('Button clicked!');
+      return Template.instance().sortedWinners;
 
 
     }
