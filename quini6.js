b4d659b146aee361b47b60b2c3dd5d5ac65775ca
German Arduino <garduino@gmail.com>
2015-09-13 13:00:26 -0300

    Intermediate Saving.

--- quini6.js
+++ quini6.js
@@ -1,47 +1,55 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  //Session.setDefault('winners', 0);
+
+function calculateWinners() {
+
+  var winners = [0, 0, 0, 0, 0, 0];
+
+  for (step = 0; step < 6; step++) {
+    // Calculo random según http://www.w3schools.com/jsref/jsref_random.asp
+    var number = Math.floor((Math.random() * 45) + 1);
+    winners[step] = number;
+  }
+
+  // var sortedWinners = winners.sort(ascending);
+  //console.log(sortedWinners);
+  // return sortedWinners;
+  this.sortedWinners = winners.sort(ascending);
+  return sortedWinners;
+
+  // Functions
+
+  // Order a numeric array, taken from http://www.aprenderaprogramar.com/index.php?option=com_content&view=article&id=834:funciones-arrays-javascript-push-sort-ordenar-numeros-concat-join-pop-shift-slice-splice-etc-cu01153e&catid=78:tutorial-basico-programador-web-javascript-desde-&Itemid=206
+
+  function ascending (elem1, elem2) { return elem1 - elem2; }
+
+ }
+
+
+
 
 
   Template.try.created = function () {
-    // here `this` refers to template instance
 
-    var winners = [0, 0, 0, 0, 0, 0];
+  calculateWinners();
 
-    for (step = 0; step < 6; step++) {
-      // Calculo random según http://www.w3schools.com/jsref/jsref_random.asp
-      var number = Math.floor((Math.random() * 45) + 1);
-      winners[step] = number;
-    }
 
-    // var sortedWinners = winners.sort(ascending);
-    //console.log(sortedWinners);
-    // return sortedWinners;
-    this.sortedWinners = winners.sort(ascending);
-
-    // Functions
-
-    // Order a numeric array, taken from http://www.aprenderaprogramar.com/index.php?option=com_content&view=article&id=834:funciones-arrays-javascript-push-sort-ordenar-numeros-concat-join-pop-shift-slice-splice-etc-cu01153e&catid=78:tutorial-basico-programador-web-javascript-desde-&Itemid=206
-
-    function ascending (elem1, elem2) { return elem1 - elem2; }
-
-    // return Session.get('sortedWinners');
   };
 
 
 Template.try.helpers({
-    sortedWinners: function () {
-      return Template.instance().sortedWinners;
+    sortedWinners: function (event, template) {
+      calculateWinners();
 
     }
   });
 
   Template.try.events({
-    'click button': function () {
+    'click button': function (event, template) {
       // increment the counter when button is clicked
       //Session.set('sortedWinners', Session.get('sortedWinners'));
-      // alert('Button clicked!');
-      return Template.instance().sortedWinners;
+      //return Template.instance().sortedWinners;
+      calculateWinners();
+      // event.target.show.Template.try();
 
 
     }
